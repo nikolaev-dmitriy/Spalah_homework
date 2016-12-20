0b03fb7e5799df15c4a2db946f4d94f498c2f5fc
nikolaev-dmitriy <nikolaev.dmitriy.1712@gmail.com>
2016-12-20 16:57:26 +0200

    Changed Calculator

--- Array(min,max,average)/src/Array.java
+++
@@ -1,7 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * Created by Man on 19.12.2016.
- */
-public class Array {
-}
--- ConsoleCalculator/src/Calculator.java
+++ ConsoleCalculator/src/Calculator.java
@@ -5,76 +5,70 @@ import java.util.Scanner;
  * Created by Дмитрий on 15.12.2016.
  */
 public class Calculator {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        double a;
-        double b=-1;
-        String act;
-        Scanner in=new Scanner(System.in);
-        String[] arr=new String[3];
-        String input="";
-        double result=0;
+        String input = "";
         System.out.println("Start Calculator");
-        Boolean t=true;
-        while (t)
-        {
-            input=in.nextLine();
-            if (input.equalsIgnoreCase("exit")){
-                t=false;
-                break;
-            }
-            else{
-            arr=input.split(" ");
-            if (arr.length==2){
-                a=result;
-                b=Double.parseDouble(arr[1]);
-                act=arr[0];
-            }
-            else{
-                result=0;
-                a=Double.parseDouble(arr[0]);
-                b=Double.parseDouble(arr[2]);
-                act=arr[1];
-            }
-            switch(act){
-            case "+":{
-                    result=a+b;
-                //System.out.println("Result = "+result);
-
-                break;
-                }
-                case "-":{
-                    result=a-b;
-                  //  System.out.println("Result = "+result);
-
-
+        calculation(input);
+    }
+        public static void calculation(String input) {
+            double a;
+            double b = -1;
+            String act;
+            double result = 0;
+            Scanner in = new Scanner(System.in);
+            String[] arr = new String[3];
+            Boolean t = true;
+            while (t) {
+                input = in.nextLine();
+                if (input.equalsIgnoreCase("exit")) {
+                    t = false;
                     break;
-                }
-                case "*":{
-                    result=a*b;
-                    //System.out.println("Result = "+result);
-
-
-                    break;
-                }
-                case "/":{
-                    result=a/b;
-                    //System.out.println("Result = "+result);
-
-                    break;
-                }
-                default:{
-
-                        break;
+                } else {
+                    arr = input.split(" ");
+                    if (arr.length == 2) {
+                        a = result;
+                        b = Double.parseDouble(arr[1]);
+                        act = arr[0];
+                    } else {
+                        result = 0;
+                        a = Double.parseDouble(arr[0]);
+                        b = Double.parseDouble(arr[2]);
+                        act = arr[1];
                     }
+                    switch (act) {
+                        case "+": {
+                            result = a + b;
+                            break;
+                        }
+                        case "-": {
+                            result = a - b;
+                            break;
+                        }
+                        case "*": {
+                            result = a * b;
+                            break;
+                        }
+                        case "/": {
+                            result = a / b;
+                            break;
+                        }
+                        default: {
 
+                            break;
+                        }
+                    }
+                    out(result);
                 }
+            }
+        }
+                public static void out (double result) {
                 DecimalFormat df = new DecimalFormat("#.###");
                 System.out.println("Result = " + df.format(result));
             }
         }
 
-        }
-    }
+
+
+
 
